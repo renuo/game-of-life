3cff1c0282ef03a56a52628bfccfe8377b6dd406
Roger Rüttimann <roger.ruettimann@renuo.ch>
2015-11-09 16:06:25 +0100

    fix bug.

--- 4-roger-cyril/game.rb
+++ 4-roger-cyril/game.rb
@@ -6,19 +6,20 @@ class Game
   end
 
   def tick
-    fields.map.with_index do |row, r|
+    @fields  = fields.map.with_index do |row, r|
       row.map.with_index do |cell, c|
         neighbours = count_active_neighbours(r, c)
         cell ? neighbours == 2 || neighbours == 3 : neighbours == 3
       end
     end
+
   end
 
   def count_active_neighbours(r, c)
     counter = 0
 
-    ([r - 1, 0].max...[fields.size - 1, r + 1].min).each do |row|
-      ([c - 1, 0].max...[fields.size[row] - 1, c + 1].min).each do |col|
+    ([r - 1, 0].max ..[fields.size - 1, r + 1].min).each do |row|
+      ([c - 1, 0].max ..[fields.size - 1, c + 1].min).each do |col|
         counter += 1 if fields[row][col] && !(row == r && col == c)
       end
     end
