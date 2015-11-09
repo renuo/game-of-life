aa840b62246aaf97ae1545402bd26c155ec9f79d
Zora Fuchs <zora.fuchs@renuo.ch>
2015-11-09 16:08:05 +0100

    ruby-version

--- Gemfile
+++ Gemfile
@@ -1,4 +1,4 @@
 source 'https://rubygems.org'
-ruby '2.2.3'
+ruby File.read('.ruby-version').strip
 
 gem 'highline'
--- visualize.rb
+++ visualize.rb
@@ -7,6 +7,10 @@ window = Window.new
 
 game = nil
 
+def random_field(size = 30, alive_ratio = 0.5)
+  size.times.to_a.map { |row| size.times.to_a.map { rand < alive_ratio } }
+end
+
 if ENV['GAME_PATH']
   require_relative(ENV['GAME_PATH'])
   game = Game.new(random_field)
@@ -20,6 +24,3 @@ loop do
   sleep(1)
 end
 
-def random_field(size = 30, alive_ratio = 0.5)
-  size.times.to_a.map { |row| size.times.to_a.map { rand < alive_ratio } }
-end
