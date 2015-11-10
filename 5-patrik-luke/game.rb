a962bc4eb788d28c8c2f3196d000581078d9c404
Lukas Elmer <lukas.elmer@gmail.com>
2015-11-10 02:44:10 +0100

    undo changes to original version

--- 5-patrik-luke/game.rb
+++ 5-patrik-luke/game.rb
@@ -2,38 +2,59 @@ require 'matrix'
 
 class Game
   def initialize(fields)
-    @matrix = Matrix[*fields].map { |cell| cell ? 1 : 0 }
-    @shifted_identity_matrix = shifted_identity_matrix
-    @shifted_identity_matrix_t = @shifted_identity_matrix.transpose
+    @fields = fields
   end
 
   def tick
-    m = @matrix
+    zero_one_array = fields.map { |row| row.map { |cell| cell ? 1 : 0 } }
 
-    u = @shifted_identity_matrix * m
-    d = @shifted_identity_matrix_t * m
-    l = m * @shifted_identity_matrix_t
-    r = m * @shifted_identity_matrix
-    ru = @shifted_identity_matrix * r
-    lu = @shifted_identity_matrix * l
-    rd = @shifted_identity_matrix_t * r
-    ld = @shifted_identity_matrix_t * l
+    m = Matrix[*zero_one_array]
 
-    blurred_matrix = (m * 0.5) + l + r + u + d + lu + ld + ru + rd
-    @matrix = blurred_matrix.map do |cell|
-      cell > 2.25 && cell < 3.75 ? 1 : 0
+    r = m.clone.to_a
+    r << r.shift
+    r = Matrix[*r]
+
+    l = m.clone.to_a
+    l.unshift l.pop
+    l = Matrix[*l]
+
+    u = m.clone.to_a
+    u.map { |row| row << row.shift }
+    u = Matrix[*u]
+
+    d = m.clone.to_a
+    d.map { |row| row.unshift row.pop }
+    d = Matrix[*d]
+
+    ru = m.clone.to_a
+    ru << ru.shift
+    ru.map { |row| row << row.shift }
+    ru = Matrix[*ru]
+
+    lu = m.clone.to_a
+    lu.unshift lu.pop
+    lu.map { |row| row << row.shift }
+    lu = Matrix[*lu]
+
+    rd = m.clone.to_a
+    rd << rd.shift
+    rd.map { |row| row.unshift row.pop }
+    rd = Matrix[*rd]
+
+    ld = m.clone.to_a
+    ld.unshift ld.pop
+    ld.map { |row| row.unshift row.pop }
+    ld = Matrix[*ld]
+
+    magic_matrix = (m * 0.5) + l + r + u + d + lu + ld + ru + rd
+    @fields = magic_matrix.to_a.map do |row|
+      row.map do |cell|
+        cell < 3.75 && cell > 2.25
+      end
     end
   end
 
   def fields
-    @matrix.map { |cell| cell == 1 }.to_a
-  end
-
-  private
-
-  def shifted_identity_matrix
-    Matrix.build(@matrix.row_count, @matrix.column_count) do |row, col|
-      row + 1 == col ? 1 : 0
-    end
+    @fields
   end
 end
