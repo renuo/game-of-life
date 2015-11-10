cf333a62d23195557e87edf13b2a688159d4d359
Lukas Elmer <lukas.elmer@gmail.com>
2015-11-10 01:37:54 +0100

    refactoring

--- 5-patrik-luke/game.rb
+++ 5-patrik-luke/game.rb
@@ -2,38 +2,38 @@ require 'matrix'
 
 class Game
   def initialize(fields)
-    @fields = fields
+    @matrix = Matrix[*fields].map { |cell| cell ? 1 : 0 }
     @shifted_identity_matrix = shifted_identity_matrix
     @shifted_identity_matrix_t = @shifted_identity_matrix.transpose
   end
 
-  def shifted_identity_matrix
-    Matrix.build(@fields.size, @fields[0].size) do |row, col|
-      row + 1 == col ? 1 : 0
+  def tick
+    m = @matrix
+
+    u = @shifted_identity_matrix * m
+    d = @shifted_identity_matrix_t * m
+    l = m * @shifted_identity_matrix_t
+    r = m * @shifted_identity_matrix
+    ru = @shifted_identity_matrix * r
+    lu = @shifted_identity_matrix * l
+    rd = @shifted_identity_matrix_t * r
+    ld = @shifted_identity_matrix_t * l
+
+    blurred_matrix = (m * 0.5) + l + r + u + d + lu + ld + ru + rd
+    @matrix = blurred_matrix.map do |cell|
+      cell > 2.25 && cell < 3.75 ? 1 : 0
     end
   end
 
-  def tick
-    m = Matrix[*fields].map { |cell| cell ? 1 : 0 }
-
-    si = @shifted_identity_matrix
-    sit = @shifted_identity_matrix_t
-    u = si * m
-    d = sit * m
-    l = m * sit
-    r = m * si
-    ru = si * r
-    lu = si * l
-    rd = sit * r
-    ld = sit * l
-
-    magic_matrix = (m * 0.5) + l + r + u + d + lu + ld + ru + rd
-    @fields = magic_matrix.map do |cell|
-      cell < 3.75 && cell > 2.25
-    end.to_a
+  def fields
+    @matrix.map { |cell| cell == 1 }.to_a
   end
 
-  def fields
-    @fields
+  private
+
+  def shifted_identity_matrix
+    Matrix.build(@matrix.row_count, @matrix.column_count) do |row, col|
+      row + 1 == col ? 1 : 0
+    end
   end
 end
