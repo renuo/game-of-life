96da5124c01012e6ede79d1a16dd9bbfe7e370e0
Lukas Elmer <lukas.elmer@renuo.ch>
2016-10-05 16:37:36 +0100

    zombie apocalypse

--- 12-josua-luke/game.rb
+++ 12-josua-luke/game.rb
@@ -1,20 +1,39 @@
 class Game
   def initialize(fields)
+    @alive_fields = []
     @fields = fields
   end
 
   def tick
     @fields = @fields.map.with_index do |row, x|
+      @alive_fields[x] ||= []
       row.map.with_index do |field, y|
-        count = count_alive_neighbours(x, y)
-        will_live?(field, count)
+        @alive_fields[x][y] ||= 0
+        will_live = will_live?(field, x, y)
+
+        if @alive_fields[x][y] >= 3 && !will_live
+          @fields[x][y] = :zombie
+        end
+
+        if will_live
+          @alive_fields[x][y] += 1
+        else
+          @alive_fields[x][y] = 0
+        end
+
+        will_live
       end
     end
   end
 
-  def will_live?(living, count)
+  def will_live?(living, x, y)
+    count = count_alive_neighbours(x, y)
     if living
-      stays_alive?(count)
+      if count_zombie_neighbours(x, y) > 0
+        return :zombie
+      else
+        return stays_alive?(count)
+      end
     else
       becomes_alive?(count)
     end
@@ -30,6 +49,10 @@ class Game
 
   def count_alive_neighbours(x, y)
     neighbours(x, y).count(true)
+  end
+
+  def count_zombie_neighbours(x, y)
+    neighbours(x, y).count(:zombie)
   end
 
   def neighbours(x, y)
