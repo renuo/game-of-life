46ee962649f7c23cc2f40fefa9cbdf992d936b4b
Josua Schmid <josua.schmid@renuo.ch>
2016-10-05 12:02:07 +0100

    correct

--- 9-josua-matthias/game.rb
+++ 9-josua-matthias/game.rb
@@ -1,12 +1,14 @@
 class Game
   def initialize(fields)
     @fields = fields
+    @x_range = 0..@fields.count-1
+    @y_range = 0..@fields[0].count-1
   end
 
   def tick
-    @fields.each_with_index do |row, i|
-      row.each_index do |j|
-        @fields[i][j] = new_state(i, j)
+    @fields = @fields.map.with_index do |row, i|
+      row.map.with_index do |_cell, j|
+        new_state(i, j)
       end
     end
   end
@@ -20,17 +22,19 @@ class Game
   def new_state(i, j)
     arr = [-1,0,1].map do | x |
       [-1,0,1].map do | y |
-        (x==0 && y==0) ? false : @fields[i+x][j+y] rescue false
+        (x==0 && y==0) ? false : @fields[i+x][j+y] if in_range(x+i, y+j)
       end
     end.flatten
 
     neighbors_count = arr.count{ |state| state }
 
-    return false if neighbors_count < 2
-    return false if neighbors_count > 3
-    return true if (neighbors_count == 2 || neighbors_count == 3) && @fields[i][j]
-    return true if neighbors_count == 3 && !@fields[i][j]
+    return false if neighbors_count < 2 || neighbors_count > 3
+    return true if neighbors_count == 3
+    @fields[i][j]
   end
 
+  def in_range(x,y)
+    [*@x_range].product([*@y_range]).include?([x,y])
+  end
 
 end
